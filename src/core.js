ad2fa46a80fd81f9766e2cab719c1e1262eeafeb
michael <michael8090@gmail.com>
2014-04-28 01:16:31 +0800

    add multiple mixin

--- src/core.js
+++ src/core.js
@@ -5,7 +5,7 @@
 define(function() {
     var GLOBAL = window || self,
         FUNCTION_KEY = '___name___',
-        CONSTRUCTOR_KEY = '___constructor___';
+        PROTOTYPE_KEY = '___prototype___';
     function _forEach(obj, method, context) {
         context = context || GLOBAL;
         if (obj instanceof Object) {
@@ -25,11 +25,52 @@ define(function() {
             throw 'Could only iterate an Object or Array';
         }
     }
-    function _mixin(dest, src) {
-        _forEach(src, function(value, key) {
-            dest[key] = value;
-        });
+    function _copy(dest) {
+        var args = arguments,
+            len = args.length,
+            i,
+            copyValue = function(value, key) {
+                dest[key] = value;
+            };
+        for (i = 1; i < len ; i++) {
+            _forEach(args[i], copyValue);
+        }
+
         return dest;
+    }
+
+    function _markFunction(config, prototype) {
+        _forEach(config, function(value, key) {
+            if (value instanceof Function) {
+                Object.defineProperty(value, FUNCTION_KEY, {
+                    configurable: false,
+                    writable: false,
+                    value: key
+                });
+                Object.defineProperty(value, PROTOTYPE_KEY, {
+                    configurable: false,
+                    writable: false,
+                    value: prototype
+                });
+            }
+        });
+    }
+
+    function _mixin() {
+        var args = [].slice.call(arguments),
+            len = args.length,
+            i,
+            p,
+            n;
+        for (i = 0; i < len - 1; i++) {
+            p = args[i];
+            n = args[i + 1];
+            n = _copy(Object.create(p), n);
+            _markFunction(n, p);
+            args[i + 1] = n;
+        }
+
+        return n;
     }
 
     function _Object(config) {
@@ -45,21 +86,10 @@ define(function() {
                 this.init(config);
             },
             prototype = Object.create(new SuperClass());
-        _forEach(config, function(value, key) {
-            if (value instanceof Function) {
-                Object.defineProperty(value, FUNCTION_KEY, {
-                    configurable: false,
-                    writable: false,
-                    value: key
-                });
-                Object.defineProperty(value, CONSTRUCTOR_KEY, {
-                    configurable: false,
-                    writable: false,
-                    value: constructor
-                });
-            }
-            prototype[key] = value;
-        });
+
+        prototype = _mixin.apply(this, [prototype].concat([].slice.apply(arguments)));
+//        _markFunction(config, prototype);
+//        _copy(prototype, config);
         prototype.constructor = constructor;
         constructor.prototype = prototype;
         constructor.extend = _Object.extend;
@@ -70,15 +100,15 @@ define(function() {
         get: function() {
             var args = arguments,
                 caller = args.callee.caller,
-                constructor = caller[CONSTRUCTOR_KEY],
-                name = caller[FUNCTION_KEY],
-                prototype;
+                prototype = caller[PROTOTYPE_KEY],
+                name = caller[FUNCTION_KEY];
+//                lookupPrototype = prototype;
 
-            if (this.hasOwnProperty(name)) {
-                prototype = constructor.prototype;
-            } else {
-                prototype = Object.getPrototypeOf(constructor.prototype);
-            }
+//            if (this.hasOwnProperty(name)) {
+//                lookupPrototype = prototype;
+//            } else {
+//                lookupPrototype = Object.getPrototypeOf(prototype);
+//            }
             var superFunction = prototype[name];
             if (superFunction instanceof Function) {
                 return superFunction;
@@ -87,27 +117,13 @@ define(function() {
         }
     });
 
-    _mixin(_Object.prototype, {
+    _copy(_Object.prototype, {
         init: function(config) {
             if (!config) {
                 return ;
             }
-            var constructor = this.constructor;
-            _forEach(config, function(value, key) {
-                if (value instanceof Function) {
-                    Object.defineProperty(value, FUNCTION_KEY, {
-                        configurable: false,
-                        writable: false,
-                        value: key
-                    });
-                    Object.defineProperty(value, CONSTRUCTOR_KEY, {
-                        configurable: false,
-                        writable: false,
-                        value: constructor
-                    });
-                }
-                this[key] = value;
-            }, this);
+            _markFunction(config, this.constructor.prototype);
+            _copy(this, config);
         }
     });
 
@@ -115,8 +131,9 @@ define(function() {
         global: GLOBAL,
         hash: {
             forEach: _forEach,
-            mixin: _mixin
+            copy: _copy
         },
+//        mixin: _mixin,
         Object: _Object
     };
 });
--- src/examples/testSuper.js
+++ src/examples/testSuper.js
@@ -1,23 +1,41 @@
 /**
  * Created by michael on 2014/4/27.
  */
-require(['Bubble', 'FlyingBubble'], function (Bubble, FlyingBubble) {
+require(['../core', 'Bubble', 'FlyingBubble'], function (core, Bubble, FlyingBubble) {
     var bubble = new Bubble();
     bubble.render();
 
-    var n = 100,
-        i,
-        fly = function () {
-            this.color = 'rgba(' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ', ' + Math.random() + ')';
-            this._super();//dynamically bind
-        };
-    for (i = 0; i < n; i++) {
-        (new FlyingBubble({
-            v: {
-                x: Math.random() * 10,
-                y: Math.random() * 10
+    var n = 5,
+        i;
+    var config1 = {
+            changeColor: function() {
+                this.color = 'rgba(' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ', ' + Math.random() + ')';
             },
-            fly: fly
+            fly: function () {
+                this.changeColor();
+                this._super();
+            }
+        },
+        config2 = {
+            fly: function () {
+                this.radius += 10;
+                this._super();
+            }
+        };
+    //multiple mixin
+    var NewBubble = FlyingBubble.extend(config1, config2);
+    for (i = 0; i < n; i++) {
+        (new NewBubble({
+            v: {
+                x: Math.random() * 5,
+                y: Math.random() * 5
+            },
+            fly: function() {
+                if (Math.random() < 0.5) {
+                    this.radius = 1;//dynamically bind
+                }
+                this._super();
+            }
         })).fly();
     }
 });
